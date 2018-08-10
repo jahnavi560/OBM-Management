1a50af151c814bc95b1e6ead55e6a00e3e14ef0b
jpthacker <jpthacker@CIPL-PC77.cygnet.com>
2018-08-10 17:38:05 +0530

    Completed Download Functionality

--- src/main/java/com/example/controller/FileController.java
+++ src/main/java/com/example/controller/FileController.java
@@ -11,17 +11,21 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -34,29 +38,32 @@ import com.example.service.FileService;
 public class FileController {
 
 	@Autowired
+	ServletContext context;
+	@Autowired
 	private FileService fileService;
 
-	private static  String fpath = "E://jj-Workspace//SpringSecurityLoginTutorial-master//uploaded-files//Mysql.docx";
 
 	@Value("${spring.upload.filepath}")
 	private String UPLOADED_FOLDER;
 
-	@PostMapping("/uploadfile") // //new annotation since 4.3
+	@PostMapping("/uploadfile") 
 	public String singleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-
 		if (file.isEmpty()) {
 			redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
 			return "redirect:uploadstatus";
 		}
 		try {
+			  Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		      String userName = auth.getName();
 			byte[] bytes = file.getBytes();
-			Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+			Path path = Paths.get(UPLOADED_FOLDER+""+ file.getOriginalFilename());
+			System.out.println(path);
 			Files.write(path, bytes);
 			FileModel fileModel = new FileModel();
 			fileModel.setName(file.getOriginalFilename());
 			fileModel.setPath(path.toString());
+			fileModel.setOwner(userName);
 			fileService.saveFile(fileModel);
-
 			redirectAttributes.addFlashAttribute("fileName", path.toString());
 			redirectAttributes.addFlashAttribute("message","You successfully uploaded '" + file.getOriginalFilename() + "'");
 
@@ -71,19 +78,15 @@ public class FileController {
 		return "upload-status";
 	}
 
-	@RequestMapping("/download/{filename}")
-	public void download(@PathVariable("filename") String filename,HttpServletRequest request, HttpServletResponse response) throws IOException {
+	@RequestMapping(value="/download/{file:.+}" ,  method = RequestMethod.GET)
+	public void download(@PathVariable("file") String filename,HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-		FileModel fileModel = fileService.getFile(filename);
-		System.out.println(filename+"------------------");
-		fpath =  fileModel.getPath();
-		File file = new File(fpath);
+		File file = new File(UPLOADED_FOLDER+""+filename);
 		if (file.exists()) {
 			String mimeType = URLConnection.guessContentTypeFromName(file.getName());
 			if (mimeType == null) {
 				mimeType = "application/octet-stream";
 			}
-
 			response.setContentType(mimeType);
 			response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
 			response.setContentLength((int) file.length());
--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,9 +1,5 @@
 package com.example.controller;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -14,14 +10,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.model.User;
 import com.example.service.UserService;
