cf2bf400d99bca402c7e762f1832a6c9cd266e12
jpthacker <jpthacker@CIPL-PC77.cygnet.com>
2018-08-31 19:35:13 +0530

    Add spring security

---
+++ src/main/java/com/example/configuration/SimpleAuthenticationSuccessHandler.java
@@ -0,0 +1,47 @@
+package com.example.configuration;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+	
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest arg0, HttpServletResponse arg1, Authentication authentication)
+			throws IOException, ServletException {
+		System.out.println("onAuthenticationSuccess----");
+		Collection<? extends GrantedAuthority>  authorities = authentication.getAuthorities();
+		authorities.forEach(authority -> {
+			if(authority.getAuthority().equals("EMPLOYEE")) {
+				try {
+					redirectStrategy.sendRedirect(arg0, arg1, "/employee-login");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else if(authority.getAuthority().equals("ADMIN")) {
+				try {
+					redirectStrategy.sendRedirect(arg0, arg1, "/admin-home");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+	            throw new IllegalStateException();
+	        }
+		});
+		
+	}
+ 
+}
